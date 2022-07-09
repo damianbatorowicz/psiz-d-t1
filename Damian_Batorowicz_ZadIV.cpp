2d0a8c0fbdc580a57c252fe8c2fece63ddb388f6
damianbatorowicz <100960911+damianbatorowicz@users.noreply.github.com>
2022-07-09 20:41:53 +0200

    Updating file
    I added new lines of code

--- Damian_Batorowicz_ZadIV.cpp
+++ Damian_Batorowicz_ZadIV.cpp
@@ -23,10 +23,59 @@ int main(int argc, char** argv) {
 	log.open("log.txt", ios::out|ios::app);
 
 	string var1, var2;
+	
 	//XOR var to compare XOR between elements (difference)
 	char XOR;
+	
 	//Counter - how many bits differ
 	int counter = 0;
+	
 	//Amount - how many bits are compared
 	long amount = 0;
 
+	struct timeval begin, end;
+	
+	//If there is a file1
+	do
+	{
+		cin >> filename1;
+		file1.open( filename1, ios::in | ios::out );
+		if( file1.good() )
+    		break;
+		cout << "There is no file with that name! Repeat the name\n";
+	}while(true);
+	
+	//If there is a file2
+	do
+	{
+		cin >> filename2;
+		file2.open( filename2, ios::in | ios::out );
+		if( file2.good() )
+    		break;
+		cout << "There is no file with that name! Repeat the name\n";
+	}while(true);
+	
+	//Setting the timer
+	gettimeofday(&begin, 0);
+	
+	//Loop to count every line before one ends
+	while( !file1.eof() && !file2.eof() )
+	{
+		getline( file1, var1);
+		getline( file2, var2);
+		
+		//Loop to count 
+		for(int i=0; i<min(var1.length(),var2.length()); i++)
+		{
+			//Using XOR loop to count how many bits differ
+			XOR = var1[i] ^ var2[i];
+			while( XOR )
+			{
+				XOR = XOR & (XOR - 1);
+				counter++;
+			}
+			amount += 8;
+		}
+		
+		//Adding 8 bits per every extra element
+		counter += absolute(var1.length()-var2.length())*8;
